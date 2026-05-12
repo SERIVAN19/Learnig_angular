19aacb5e084fd15f245bfec4c15c1e0732fb4e96
SERIVAN19 <sbuenoardila@gmail.com>
2026-05-12 14:06:20 -0500

    feat work with directives

--- src/app/ventas/ventas.ts
+++ src/app/ventas/ventas.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { ProductoComponent } from '../producto/producto.component';
+import { NgClass } from '@angular/common';
+import { NgStyle } from '@angular/common';
 
 @Component({
   standalone: true,
+  imports: [ProductoComponent, NgClass, NgStyle],  //Directivas
   selector: 'app-ventas',
   templateUrl: './ventas.html',
   styleUrls: ['./ventas.css'],
 })
-export class VentasComponent {}
+export class VentasComponent {
+
+  public colorear:boolean = false;
+
+  public mostar:string = "Si";
+}
